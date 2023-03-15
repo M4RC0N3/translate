94d6e955e54f4d3271c504e629962a0bd4ac096e
Marcone Ribeiro <marcone@quickup.io>
2023-03-15 16:20:38 -0300

    refatora código
    - adiciona comentários
    - Permite que a linguagem do documento seja de acordo com o idioma do navegador
    - Testa desenpenho

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 window.onload = function() {
-    Languages.init(['en_EN', 'fr_FR'], './i18n/', () => {
-        const translateList = document.getElementsByClassName('str');
-        let words = [];
+    const getPageLang = document.getElementsByTagName('html'); // Seleciona tag html no topo do documento
 
-        console.log(translateList.length);
-        for( let jumper=0; jumper < translateList.length; jumper++){
-            words.push(translateList[jumper]);
+    const navigatorLang = navigator.languages?.length ? navigator.languages[0] : navigator.language; // Pega idioma usado no navegador do usuário
+    let defineLang = navigatorLang == 'pt-BR'? navigatorLang : 'en-US'; // Verifica se o idioma é português, caso contrário define inglês
+
+    getPageLang[0].lang = defineLang; // Define o idioma da página
+
+    Languages.init([defineLang], './i18n/', () => {
+        const translateList = document.getElementsByClassName('translate'); // Seleciona todos as tags traduzíveis do documento
+
+        let translatableElements = [];
+
+        for( let jumper=0; jumper < translateList.length; jumper++){ // adiciona cada elemento traduzível e adiciona em uma nova lista
+            translatableElements.push(translateList[jumper]);
         }
         
-        words.forEach(item=>{
+        translatableElements.forEach(item=>{ // traduz cada elemento da listagem
             item.innerHTML = item.id.t()
         })
     });
