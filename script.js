cb2a4b9fe8998ade1114dc190f618407075cfaf1
Marcone Ribeiro <marcone@quickup.io>
2023-03-15 14:51:50 -0300

    cria código para traduzir variáveis

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    Languages.init(['en_EN', 'fr_FR'], './i18n/', () => {
+        let translateList = document.getElementsByClassName('str');
+        let words = [];
+
+        console.log(translateList.length);
+        for( let jumper=0; jumper < translateList.length; jumper++){
+            words.push(translateList[jumper]);
+        }
+        
+        words.forEach(item=>{
+            item.innerHTML = item.id.t()
+        })
+    });
+}
